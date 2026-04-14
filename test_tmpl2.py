a957b38cd7cfe9826964a6ba57b163056eae2cc4
Vishrut <vishrutgundaji@gmail.com>
2026-04-14 21:58:35 +0530

    refactor: remove obsolete template testing scripts

--- test_tmpl2.py
+++
@@ -1,6 +0,0 @@
-from yt_dlp import YoutubeDL
-ydl = YoutubeDL({'outtmpl': '%(playlist_index&%s - |)s%(title)s.ext'})
-print(ydl.prepare_filename({'title': 'test', 'playlist_index': 5}))
-
-ydl2 = YoutubeDL({'outtmpl': '%(playlist_index&%02d - |)s%(title)s.ext'})
-print(ydl2.prepare_filename({'title': 'test', 'playlist_index': 5}))
